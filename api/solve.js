459e2f7b203ca70c8c376f4d5b561353eafd6f70
easymathai <zayir143@gmail.com>
2026-06-07 09:13:03 +0400

    Fix API body error

--- api/solve.js
+++ api/solve.js
@@ -1,13 +1,11 @@
 export default async function handler(req, res) {
-  const { question } = req.body;
+  const question = req.body?.question || "";
 
   let answer = "EasyMath AI is working. Full AI solver coming soon.";
 
   if (question === "25+65") {
     answer = "25 + 65 = 90";
-  }
-
-  if (question === "2x+5=17") {
+  } else if (question === "2x+5=17") {
     answer = "x = 6";
   }
 
